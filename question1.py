b6be703c7a7394395b817cf33874db9d04ba6e3b
Ropy Kevin <ropykevin@gmail.com>
2023-10-25 07:55:30 -0900

    done

--- question1.py
+++ question1.py
@@ -1,3 +1,4 @@
+print("Question 1:")
 email = input("Enter your email adress: ")
 at_index = 1
 dot_index = -1
@@ -17,3 +18,59 @@ domain=f"DOMAIN: {parts[1]}"
 print(id)
 print(domain)
 
+print('question 2:')
+email = email.lower()
+
+vowels = "aeiou"
+vowel_count = 0
+consonant_count = 0
+special_count = 0
+total_count = 0
+char_counts = {}
+
+for i in email:
+    if i.isalpha():
+        total_count += 1  # Increment the total character count
+        if i in vowels:
+            vowel_count += 1
+            char_type = 'vowel'
+        else:
+            consonant_count += 1
+            char_type = 'consonant'
+    else:
+        total_count += 1  # Increment the total character count for special characters
+        special_count += 1
+        char_type = 'special character'
+
+    # Check if the character is already in char_counts, and increment the count, or add it to the dictionary with a count of 1.
+    if i in char_counts:
+        char_counts[i][0] += 1
+    else:
+        char_counts[i] = [1, char_type]
+
+
+print(f"\nVowel Count: {vowel_count}")
+print(f"Consonant Count: {consonant_count}")
+print(f"Special Character Count: {special_count}")
+print(f"Total Character Count: {total_count}")
+
+print("\nCharacter Counts:")
+for char, (count, char_type) in char_counts.items():
+    print(f"'{char}' => {count} => {char_type}")
+
+print("question 3:")
+
+for i in email:
+    if i.isdigit():  # Checking if the character is a digit
+        num = int(i)  # Converting the character to an integer
+        if (num % 2 == 0) or (num % 3 == 0) or (num % 5 == 0):
+            print(str(num))
+
+print("question 4:")
+
+
+print("ASCII values for the characters in the email address:")
+
+for char in email:
+    ascii_value = ord(char)
+    print(f"Character: {char}, ASCII Value: {ascii_value}")
--- question2.py
+++ question2.py
@@ -1,44 +1,39 @@
-# email = input("Enter your email address: ")
-
-# vowels = "aeiou,AEIOU"
-# vowel_count = 0
-# consonant_count = 0
-# special_count = 0
-
-# for i in email:
-#     if i.isalpha():
-#         if i in vowels:
-#             vowel_count += 1
-#         else:
-#             consonant_count += 1
-#     else:
-#         special_count += 1
-
-# print("Vowels: ", vowel_count)
-# print("Consonants: ", consonant_count)
-# print("Special characters: ", special_count)
-
 email = input("Enter your email address: ")
-
-# Convert the email to lowercase for case-insensitive counting
 email = email.lower()
 
 vowels = "aeiou"
 vowel_count = 0
 consonant_count = 0
 special_count = 0
+total_count = 0
+char_counts = {}
 
 for i in email:
     if i.isalpha():
+        total_count += 1  # Increment the total character count
         if i in vowels:
             vowel_count += 1
+            char_type = 'vowel'
         else:
             consonant_count += 1
+            char_type = 'consonant'
     else:
+        total_count += 1  # Increment the total character count for special characters
         special_count += 1
+        char_type = 'special character'
 
-    print(f"{i} is a {'vowel' if i in vowels else 'consonant' if i.isalpha() else 'special character'}")
+    # Check if the character is already in char_counts, and increment the count, or add it to the dictionary with a count of 1.
+    if i in char_counts:
+        char_counts[i][0] += 1
+    else:
+        char_counts[i] = [1, char_type]
+
 
 print(f"\nVowel Count: {vowel_count}")
 print(f"Consonant Count: {consonant_count}")
 print(f"Special Character Count: {special_count}")
+print(f"Total Character Count: {total_count}")
+
+print("\nCharacter Counts:")
+for char, (count, char_type) in char_counts.items():
+    print(f"'{char}' => {count} => {char_type}")
